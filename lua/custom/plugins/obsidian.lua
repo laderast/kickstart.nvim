38d1fa4941c25a0910d83f782cee8f7862bebe20
Ted Laderas <roshioshi@gmail.com>
2025-03-30 14:36:50 -0700

    Update obsidian.lua

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -25,6 +25,29 @@ return {
       },
     },
 
+    log_level = vim.log.levels.INFO,
+
+  daily_notes = {
+    -- Optional, if you keep daily notes in a separate directory.
+    folder = "daily-notes",
+    -- Optional, if you want to change the date format for the ID of daily notes.
+    date_format = "%Y-%m-%d",
+    -- Optional, if you want to change the date format of the default alias of daily notes.
+    alias_format = "%B %-d, %Y",
+    -- Optional, default tags to add to each new daily note created.
+    default_tags = { "daily-notes" },
+    -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+    template = nil
+  },
+
+  -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
+  completion = {
+    -- Set to false to disable completion.
+    nvim_cmp = true,
+    -- Trigger completion at 2 chars.
+    min_chars = 2,
+  },
+
     -- see below for full list of options 👇
   },
 }
