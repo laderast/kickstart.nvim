a9019027b37b3f0d9e25d16f3376b5111594e6df
Ted Laderas <roshioshi@gmail.com>
2025-03-30 13:06:39 -0700

    Update init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -22,6 +22,37 @@ return {
     -- see below for full list of optional dependencies 👇
   },
 
+ opts = {
+
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/teb_zab",
+      },
+    },
+
+      log_level = vim.log.levels.INFO,
+
+  daily_notes = {
+    -- Optional, if you keep daily notes in a separate directory.
+    folder = "daily-notes",
+    -- Optional, if you want to change the date format for the ID of daily notes.
+    date_format = "%Y-%m-%d",
+    -- Optional, if you want to change the date format of the default alias of daily notes.
+    alias_format = "%B %-d, %Y",
+    -- Optional, default tags to add to each new daily note created.
+    default_tags = { "daily-notes" },
+    -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+    template = nil
+  },
+
+  -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
+  completion = {
+    -- Set to false to disable completion.
+    nvim_cmp = true,
+    -- Trigger completion at 2 chars.
+    min_chars = 2,
+  },
 
     -- see below for full list of options 👇
   },
