8d384328b42a90ec9d96a879512562e02d426623
Ted Laderas <roshioshi@gmail.com>
2025-03-30 14:37:18 -0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -141,6 +141,8 @@ vim.opt.timeoutlen = 300
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
+vim.opt.conceallevel = 2
+
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
