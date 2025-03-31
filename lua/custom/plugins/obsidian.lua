3628818ddd191c8a6e1cda3c827b35432dde50b6
Ted Laderas <roshioshi@gmail.com>
2025-03-31 07:20:20 -0700

    trying other opts

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -17,37 +17,77 @@ return {
 
     -- see below for full list of optional dependencies 👇
   },
-  opts={
-    workspaces = {
-      {
-        name = "personal",
-        path = "~/teb_zab",
-      },
-    },
-  },
-
-    log_level = vim.log.levels.INFO,
-
-  daily_notes = {
-    -- Optional, if you keep daily notes in a separate directory.
-    folder = "daily-notes",
-    -- Optional, if you want to change the date format for the ID of daily notes.
-    date_format = "%Y-%m-%d",
-    -- Optional, if you want to change the date format of the default alias of daily notes.
-    alias_format = "%B %-d, %Y",
-    -- Optional, default tags to add to each new daily note created.
-    default_tags = { "daily-notes" },
-    -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-    template = nil
-  },
-
-  -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
-  completion = {
-    -- Set to false to disable completion.
-    nvim_cmp = true,
-    -- Trigger completion at 2 chars.
-    min_chars = 2,
-    }
+  opts = {
+		workspaces = {
+			{
+				name = "notes",
+				path = "~/Documents/teb_zab",
+			},
+		},
+		completion = {
+			nvim_cmp = true,
+			min_chars = 2,
+		},
+		mappings = {
+			-- overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+			["gf"] = {
+				action = function()
+					return require("obsidian").util.gf_passthrough()
+				end,
+				opts = { noremap = false, expr = true, buffer = true },
+			},
+			-- toggle check-boxes.
+			["<leader>ch"] = {
+				action = function()
+					return require("obsidian").util.toggle_checkbox()
+				end,
+				opts = { buffer = true },
+			},
+			-- smart action depending on context, either follow link or toggle checkbox.
+			["<cr>"] = {
+				action = function()
+					return require("obsidian").util.smart_action()
+				end,
+				opts = { buffer = true, expr = true },
+			},
+		},
+		note_frontmatter_func = function(note)
+			if note.title then
+				note:add_alias(note.title)
+			end
+			local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+			-- ensure fields are kept in the frontmatter
+			if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+				for k, v in pairs(note.metadata) do
+					out[k] = v
+				end
+			end
+			return out
+		end,
+		follow_url_func = function(url)
+			vim.fn.jobstart({ "yank", url })
+		end,
+		follow_img_func = function(img)
+			vim.fn.jobstart({ "yank", img })
+		end,
+		picker = {
+			name = "telescope.nvim",
+			-- note_mappings = {
+			-- 	-- create a new note from your query.
+			-- 	new = "<C-n>",
+			-- 	-- insert a link to the selected note.
+			-- 	insert_link = "<C-l>",
+			-- },
+			-- tag_mappings = {
+			-- 	tag_note = "<C-x>",
+			-- 	insert_tag = "<C-l>",
+			-- },
+		},
+		sort_by = "modified",
+		sort_reversed = true,
+		search_max_lines = 1000,
+		open_notes_in = "current", -- vsplit|hsplit
+	},
   }
     -- see below for full list of options 👇
 
